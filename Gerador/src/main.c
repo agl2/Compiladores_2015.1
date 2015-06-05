23076c39bf5451bea7f3f982093b98926e990270
Ary Lins <agl2@cin.ufpe.br>
2015-06-05 12:31:29 -0300

    analisando bug

--- Gerador/src/main.c
+++ Gerador/src/main.c
@@ -4,11 +4,13 @@
 
 int main()
 {
-    int numY = 200;
-    printf("Numero de repeticoes de Y: "); scanf("%d", &numY);
+    int numY;
+    printf("Numero de repeticoes de Y: ");
+    scanf("%d", &numY);
     int i=0;
-    char valordeY[1000] = "s(cBI(SII))I(B(cBI(SII))I)";
-
+    char valordeY[1000] = "S(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))"; 
+    //s(cBI(SII))I(B(cBI(SII))I)
+    //"S(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))(S(S(KS)(S(KK)(SKK)))(K(S(SKK)(SKK))))"
     FILE *entradagerada;
     entradagerada = fopen ("in.txt", "w");
 
