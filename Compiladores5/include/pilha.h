178eab7d0ef196bdb30a75d2b0086ff24d03ff2f
Ary Lins <agl2@cin.ufpe.br>
2015-06-06 12:31:46 -0300

    adding bin/executavel

--- Compiladores5/include/pilha.h
+++ Compiladores5/include/pilha.h
@@ -2,11 +2,10 @@
 #define PILHA_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
-#include "../include/arvore.h"
+#include "arvore.h"
 #define TIPO Celula*
 
-typedef struct Pilha 
-{
+typedef struct Pilha {
     TIPO* dados;
     int tamanho;
     int ponteiro_posicao;
@@ -17,8 +16,7 @@ typedef struct Pilha
 inline Pilha* pilha_cria();
 inline void pilha_insere(Pilha* pl, TIPO elemento);
 inline TIPO pilha_topo(Pilha* p);
-inline int pilha_vazia(Pilha* p);
-inline void pilha_destroi(Pilha* p);
-inline void pilha_retira(Pilha* p);
+inline int pilha_vazia(Pilha*);
+inline void pilha_destroi(Pilha*);
+inline void pilha_retira(Pilha*);
 #endif // PILHA_H_INCLUDED
-
